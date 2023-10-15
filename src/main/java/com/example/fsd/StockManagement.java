68681a24ece27c9a1930ba7f859623653abcf62f
brunoalves8 <ba578731@gmail.com>
2023-10-15 14:23:29 +0100

    melhorias no código

--- src/main/java/com/example/fsd/StockManagement.java
+++ src/main/java/com/example/fsd/StockManagement.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.util.*;
 
 public class StockManagement {
+    private static final Object fileLock = new Object();
 
     public static List<String> getAllStockProductsList(String filePath) {
         List<String> produtos = new ArrayList<>();
@@ -24,7 +25,7 @@ public class StockManagement {
                 int quantidade = Integer.parseInt(record.get("Quantidade"));
 
                 if (quantidade > 0) {
-                    produtos.add(id+"   "+nome+" -> QTD:"+qtd);
+                    produtos.add(id + "   " + nome + " -> QTD:" + qtd);
                 }
             }
         } catch (Exception e) {
@@ -33,70 +34,75 @@ public class StockManagement {
 
         return produtos;
     }
+
     public static void addProductQuantity(String filePath, String productId, int quantityToAdd) {
-        List<CSVRecord> recordsList = new ArrayList<>();
+        synchronized (fileLock) {
+            List<CSVRecord> recordsList = new ArrayList<>();
 
-        try (Reader in = new FileReader(filePath)) {
-            CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
-            recordsList.addAll(parser.getRecords());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Adicionar quantidade baseado no ID do produto
-        List<Map<String, String>> updatedRecordsMap = new ArrayList<>();
-        for (CSVRecord record : recordsList) {
-            Map<String, String> recordMap = record.toMap();
-            if (record.get("ID").equals(productId)) {
-                int currentQuantity = Integer.parseInt(record.get("Quantidade"));
-                int updatedQuantity = currentQuantity + quantityToAdd;
-                recordMap.put("Quantidade", String.valueOf(updatedQuantity));
+            try (Reader in = new FileReader(filePath)) {
+                CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+                recordsList.addAll(parser.getRecords());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            updatedRecordsMap.add(recordMap);
-        }
+
+            // Adicionar quantidade baseado no ID do produto
+            List<Map<String, String>> updatedRecordsMap = new ArrayList<>();
+            for (CSVRecord record : recordsList) {
+                Map<String, String> recordMap = record.toMap();
+                if (record.get("ID").equals(productId)) {
+                    int currentQuantity = Integer.parseInt(record.get("Quantidade"));
+                    int updatedQuantity = currentQuantity + quantityToAdd;
+                    recordMap.put("Quantidade", String.valueOf(updatedQuantity));
+                }
+                updatedRecordsMap.add(recordMap);
+            }
 
 // Reescrever o arquivo CSV
-        try (Writer out = new FileWriter(filePath)) {
-            CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Nome", "Quantidade").print(out);
-            for (Map<String, String> recordMap : updatedRecordsMap) {
-                printer.printRecord(recordMap.values());
+            try (Writer out = new FileWriter(filePath)) {
+                CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Nome", "Quantidade").print(out);
+                for (Map<String, String> recordMap : updatedRecordsMap) {
+                    printer.printRecord(recordMap.values());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
     public static void removeProductQuantity(String filePath, String productId, int quantityToAdd) {
-        List<CSVRecord> recordsList = new ArrayList<>();
+        synchronized (fileLock) {
+            List<CSVRecord> recordsList = new ArrayList<>();
 
-        try (Reader in = new FileReader(filePath)) {
-            CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
-            recordsList.addAll(parser.getRecords());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Adicionar quantidade baseado no ID do produto
-        List<Map<String, String>> updatedRecordsMap = new ArrayList<>();
-        for (CSVRecord record : recordsList) {
-            Map<String, String> recordMap = record.toMap();
-            if (record.get("ID").equals(productId)) {
-                int currentQuantity = Integer.parseInt(record.get("Quantidade"));
-                int updatedQuantity = currentQuantity - quantityToAdd;
-                recordMap.put("Quantidade", String.valueOf(updatedQuantity));
+            try (Reader in = new FileReader(filePath)) {
+                CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+                recordsList.addAll(parser.getRecords());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            updatedRecordsMap.add(recordMap);
-        }
+
+            // Adicionar quantidade baseado no ID do produto
+            List<Map<String, String>> updatedRecordsMap = new ArrayList<>();
+            for (CSVRecord record : recordsList) {
+                Map<String, String> recordMap = record.toMap();
+                if (record.get("ID").equals(productId)) {
+                    int currentQuantity = Integer.parseInt(record.get("Quantidade"));
+                    int updatedQuantity = currentQuantity - quantityToAdd;
+                    recordMap.put("Quantidade", String.valueOf(updatedQuantity));
+                }
+                updatedRecordsMap.add(recordMap);
+            }
 
 // Reescrever o arquivo CSV
-        try (Writer out = new FileWriter(filePath)) {
-            CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Nome", "Quantidade").print(out);
-            for (Map<String, String> recordMap : updatedRecordsMap) {
-                printer.printRecord(recordMap.values());
+            try (Writer out = new FileWriter(filePath)) {
+                CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Nome", "Quantidade").print(out);
+                for (Map<String, String> recordMap : updatedRecordsMap) {
+                    printer.printRecord(recordMap.values());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-    }
+}
 
