96cddbe7da0574146f0164029d764c2e87b151ee
brunoalves8 <ba578731@gmail.com>
2023-11-13 01:01:54 +0000

    VERSÃO FINAL

--- src/main/java/com/example/fsd/DirectNotificationImpl.java
+++ src/main/java/com/example/fsd/DirectNotificationImpl.java
@@ -5,8 +5,8 @@ import java.rmi.RemoteException;
 public class DirectNotificationImpl implements DirectNotification {
     @Override
     public String Stock_updated(String message) throws RemoteException {
-        System.out.println("Atualização de stock: " + message);
-        return "Atualização de stock: " + message;
+        System.out.println("Atualização de stock: \n" + message);
+        return "Atualização de stock: \n" + message;
     }
 }
 
